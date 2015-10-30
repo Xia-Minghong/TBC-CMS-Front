1b6d3609dd1c2414fce12f0e5d2b197372fc8b67
simonxiang <xiangqk_001@hotmail.com>
2015-10-30 15:19:53 +0800

    Slight modification

--- app/scripts/controllers/js/create-crisis.js
+++ app/scripts/controllers/js/create-crisis.js
@@ -43,6 +43,12 @@ function createCrisisInit($scope) {
         $("input#timeHidden").val(t.toUTCString()).trigger("change");
     });
 
+    window.setTimeout(function() {
+        var date = new Date();
+        date.setSeconds(0);
+        $("input#timeHidden").val(date.toUTCString()).trigger("change");
+    },500);
+
     var select = $('select#locationAddress');
     select.placecomplete({
         placeholderText: "Enter address",
