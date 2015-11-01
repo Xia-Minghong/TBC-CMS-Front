8c4c60c863ee6653e1054115384200a760f42a74
simonxiang <xiangqk_001@hotmail.com>
2015-11-01 19:45:57 +0800

    Map details modal design

--- app/scripts/controllers/js/map-incident-modal.js
+++ app/scripts/controllers/js/map-incident-modal.js
@@ -3,6 +3,22 @@ function initMapIncidentModal($scope) {
     var mmt = moment(timeStr, "YYYY-MM-DDThh:mm:ssZ");
     $scope.incident.time = mmt.format("DD/MM/YYYY hh:mm");
 
+    var dispatches = $scope.incident['dispatch_set'];
+
+    for (var i = 0; i < dispatches.length; i++) {
+        timeStr = dispatches[i].time;
+        mmt = moment(timeStr, "YYYY-MM-DDThh:mm:ssZ");
+        dispatches[i].time = mmt.format("hh:mm");
+    }
+
+    var updates = $scope.incident['inciupdate_set'];
+
+    for (var i = 0; i < updates.length; i++) {
+        timeStr = updates[i].time;
+        mmt = moment(timeStr, "YYYY-MM-DDThh:mm:ssZ");
+        updates[i].time = mmt.format("hh:mm");
+    }
+
     setTimeout(function() {
         var ichecks = $('div#event-severity-check input[type="checkbox"].flat-red, input[type="radio"].flat-red');
         ichecks.iCheck({
--- app/scripts/crisis_map.js
+++ app/scripts/crisis_map.js
@@ -13,7 +13,7 @@ var incidentMarkers = {
     haze: [],
     crash: [],
     dengue: []
-}
+};
 
 
 function initMap($rootScope) {
@@ -45,110 +45,7 @@ function initMap($rootScope) {
                     map.panTo(lastValidCenter);
                 });
 
-                // Clear global lists
-                incidents = {
-                    fire: [],
-                    haze: [],
-                    crash: [],
-                    dengue: []
-                };
-
-                incidentMarkers = {
-                    fire: [],
-                    haze: [],
-                    crash: [],
-                    dengue: []
-                };
-
-                var incidentList = $rootScope.incidents;
-
-                for (var i = 0; i < incidentList.length; i++) {
-                    var incident = incidentList[i];
-                    var marker = new google.maps.Marker({
-                        position: {lat: parseFloat(incident.latitude), lng: parseFloat(incident.longitude)},
-                        title: incident.name,
-                        icon: 'images/' + incident.type + '-pin-' + Math.min(5, incident.severity) + '.png'
-                    });
-                    incident["marker"] = marker;
-                    marker.incident = incident;
-
-                    switch (incident.type) {
-                        case "fire":
-                            incidents.fire.push(incident);
-                            incidentMarkers.fire.push(marker);
-                            break;
-                        case "haze":
-                            incidents.haze.push(incident);
-                            incidentMarkers.haze.push(marker);
-                            break;
-                        case "crash":
-                            incidents.crash.push(incident);
-                            incidentMarkers.crash.push(marker);
-                            break;
-                        case "dengue":
-                            incidents.dengue.push(incident);
-                            incidentMarkers.dengue.push(marker);
-                            break;
-                    }
-
-                    marker.setMap(map);
-
-                    google.maps.event.addListener(marker, "click", function(event) {
-                        //alert(this.incident.id);
-                      $rootScope.openMapModal(this.incident.id);
-                    });
-                }
-
-                var toggleMarkers = function(list, show) {
-                    for (var i = 0; i < list.length; i++) {
-                        list[i].setMap(show ? map : null);
-                    }
-                };
-
-                $("div#map-label-all").click(function() {
-                    toggleMarkers(incidentMarkers.fire, true);
-                    toggleMarkers(incidentMarkers.haze, true);
-                    toggleMarkers(incidentMarkers.crash, true);
-                    toggleMarkers(incidentMarkers.dengue, true);
-                    $("div.map-label").removeClass("active");
-                    $(this).addClass("active");
-                });
-
-                $("div#map-label-fire").click(function() {
-                    toggleMarkers(incidentMarkers.fire, true);
-                    toggleMarkers(incidentMarkers.haze, false);
-                    toggleMarkers(incidentMarkers.crash, false);
-                    toggleMarkers(incidentMarkers.dengue, false);
-                    $("div.map-label").removeClass("active");
-                    $(this).addClass("active");
-                }).find("div.map-label-number").text(incidents.fire.length);
-
-                $("div#map-label-haze").click(function() {
-                    toggleMarkers(incidentMarkers.fire, false);
-                    toggleMarkers(incidentMarkers.haze, true);
-                    toggleMarkers(incidentMarkers.crash, false);
-                    toggleMarkers(incidentMarkers.dengue, false);
-                    $("div.map-label").removeClass("active");
-                    $(this).addClass("active");
-                }).find("div.map-label-number").text(incidents.haze.length);
-
-                $("div#map-label-crash").click(function() {
-                    toggleMarkers(incidentMarkers.fire, false);
-                    toggleMarkers(incidentMarkers.haze, false);
-                    toggleMarkers(incidentMarkers.crash, true);
-                    toggleMarkers(incidentMarkers.dengue, false);
-                    $("div.map-label").removeClass("active");
-                    $(this).addClass("active");
-                }).find("div.map-label-number").text(incidents.crash.length);
-
-                $("div#map-label-dengue").click(function() {
-                    toggleMarkers(incidentMarkers.fire, false);
-                    toggleMarkers(incidentMarkers.haze, false);
-                    toggleMarkers(incidentMarkers.crash, false);
-                    toggleMarkers(incidentMarkers.dengue, true);
-                    $("div.map-label").removeClass("active");
-                    $(this).addClass("active");
-                }).find("div.map-label-number").text(incidents.dengue.length);
+                resetMarkers($rootScope);
 
             }, 300);
 
@@ -183,4 +80,110 @@ function initMap($rootScope) {
             }, 300);
         }
     }, 600);
+}
+
+function resetMarkers($rootScope) {
+    // Clear global lists
+    incidents = {
+        fire: [],
+        haze: [],
+        crash: [],
+        dengue: []
+    };
+
+    incidentMarkers = {
+        fire: [],
+        haze: [],
+        crash: [],
+        dengue: []
+    };
+
+    var incidentList = $rootScope.incidents;
+
+    for (var i = 0; i < incidentList.length; i++) {
+        var incident = incidentList[i];
+        var marker = new google.maps.Marker({
+            position: {lat: parseFloat(incident.latitude), lng: parseFloat(incident.longitude)},
+            title: incident.name,
+            icon: 'images/' + incident.type + '-pin-' + Math.min(5, incident.severity) + '.png'
+        });
+        incident["marker"] = marker;
+        marker.incident = incident;
+
+        switch (incident.type) {
+            case "fire":
+                incidents.fire.push(incident);
+                incidentMarkers.fire.push(marker);
+                break;
+            case "haze":
+                incidents.haze.push(incident);
+                incidentMarkers.haze.push(marker);
+                break;
+            case "crash":
+                incidents.crash.push(incident);
+                incidentMarkers.crash.push(marker);
+                break;
+            case "dengue":
+                incidents.dengue.push(incident);
+                incidentMarkers.dengue.push(marker);
+                break;
+        }
+
+        marker.setMap(map);
+
+        google.maps.event.addListener(marker, "click", function(event) {
+            $rootScope.openMapModal(this.incident.id);
+        });
+    }
+
+    var toggleMarkers = function(list, show) {
+        for (var i = 0; i < list.length; i++) {
+            list[i].setMap(show ? map : null);
+        }
+    };
+
+    $("div#map-label-all").click(function() {
+        toggleMarkers(incidentMarkers.fire, true);
+        toggleMarkers(incidentMarkers.haze, true);
+        toggleMarkers(incidentMarkers.crash, true);
+        toggleMarkers(incidentMarkers.dengue, true);
+        $("div.map-label").removeClass("active");
+        $(this).addClass("active");
+    });
+
+    $("div#map-label-fire").click(function() {
+        toggleMarkers(incidentMarkers.fire, true);
+        toggleMarkers(incidentMarkers.haze, false);
+        toggleMarkers(incidentMarkers.crash, false);
+        toggleMarkers(incidentMarkers.dengue, false);
+        $("div.map-label").removeClass("active");
+        $(this).addClass("active");
+    }).find("div.map-label-number").text(incidents.fire.length);
+
+    $("div#map-label-haze").click(function() {
+        toggleMarkers(incidentMarkers.fire, false);
+        toggleMarkers(incidentMarkers.haze, true);
+        toggleMarkers(incidentMarkers.crash, false);
+        toggleMarkers(incidentMarkers.dengue, false);
+        $("div.map-label").removeClass("active");
+        $(this).addClass("active");
+    }).find("div.map-label-number").text(incidents.haze.length);
+
+    $("div#map-label-crash").click(function() {
+        toggleMarkers(incidentMarkers.fire, false);
+        toggleMarkers(incidentMarkers.haze, false);
+        toggleMarkers(incidentMarkers.crash, true);
+        toggleMarkers(incidentMarkers.dengue, false);
+        $("div.map-label").removeClass("active");
+        $(this).addClass("active");
+    }).find("div.map-label-number").text(incidents.crash.length);
+
+    $("div#map-label-dengue").click(function() {
+        toggleMarkers(incidentMarkers.fire, false);
+        toggleMarkers(incidentMarkers.haze, false);
+        toggleMarkers(incidentMarkers.crash, false);
+        toggleMarkers(incidentMarkers.dengue, true);
+        $("div.map-label").removeClass("active");
+        $(this).addClass("active");
+    }).find("div.map-label-number").text(incidents.dengue.length);
 }
